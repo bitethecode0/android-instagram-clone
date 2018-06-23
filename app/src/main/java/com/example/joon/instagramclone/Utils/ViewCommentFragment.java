32dbc80288ad8ddf69ca922b00da8c211eaf9e63
jooncsguy <joon.csguy@gmail.com>
2018-06-23 12:51:53 -0500

    2018.6.23 12:50pm, 			New method 'private void newComment'
                            	,Query again to insert comment into the firebase
                            	,setOnclick Listener to the check button

--- app/src/main/java/com/example/joon/instagramclone/Utils/ViewCommentFragment.java
+++ app/src/main/java/com/example/joon/instagramclone/Utils/ViewCommentFragment.java
@@ -1,5 +1,6 @@
 package com.example.joon.instagramclone.Utils;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,8 +17,16 @@ import android.widget.ListView;
 import com.example.joon.instagramclone.Model.Comment;
 import com.example.joon.instagramclone.Model.Photo;
 import com.example.joon.instagramclone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class ViewCommentFragment extends Fragment {
     private static final String TAG = "ViewCommentFragment";
@@ -25,6 +35,13 @@ public class ViewCommentFragment extends Fragment {
         super();
         setArguments(new Bundle());
     }
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+
 
     //widgets
     private ImageView mBackArrow, mCheckMark;
@@ -47,6 +64,9 @@ public class ViewCommentFragment extends Fragment {
         mListView = view.findViewById(R.id.listView);
 
         mCommentsList = new ArrayList<>();
+
+        setupFirebaseAuth();
+
         try{
             mPhoto = getPhotoFromBundle();
         } catch (NullPointerException e){
@@ -59,10 +79,65 @@ public class ViewCommentFragment extends Fragment {
         firstLineComment.setDate_created(mPhoto.getDate_created());
 
         mCommentsList.add(firstLineComment);
+
         CommentListAdapter adapter = new CommentListAdapter(getActivity(), R.layout.layout_commnet, mCommentsList);
         mListView.setAdapter(adapter);
 
+        mCheckMark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!mComment.getText().toString().equals("")){
+
+                    addNewComment(mComment.getText().toString());
+                    mComment.setText("");
+                    closeKeyboard();
+                } else{
+
+                }
+            }
+        });
+
+
+
         return view;
+    }
+
+    private void closeKeyboard() {
+        View view = getActivity().getCurrentFocus();
+        if(view!=null){
+            InputMethodManager im = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void addNewComment(String newComment){
+        Log.d(TAG, "addNewComment: adding new comment "+newComment);
+        Comment comment = new Comment();
+        String commentID = myRef.push().getKey(); // generate random number
+
+        comment.setComment(newComment);
+        comment.setDate_created(getTimestamp());
+        comment.setUser_id(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        myRef.child(getString(R.string.dbname_photos))
+                .child(mPhoto.getPhoto_id())
+                .child(getString(R.string.field_comments))
+                .child(commentID)
+                .setValue(comment);
+
+        myRef.child(getString(R.string.dbname_user_photos))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(mPhoto.getPhoto_id())
+                .child(getString(R.string.field_comments))
+                .child(commentID)
+                .setValue(comment);
+
+    }
+
+    private String getTimestamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+        sdf.setTimeZone(TimeZone.getTimeZone("US/Pacific"));
+        return sdf.format(new Date());
     }
 
     private Photo getPhotoFromBundle() {
@@ -75,4 +150,53 @@ public class ViewCommentFragment extends Fragment {
             return null;
         }
     }
+
+    /**
+     * ----------------------firebase------------------------
+     */
+    /**
+     *
+     * check to see if @param user is logged in.
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebase: setting up firebase auth");
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Log.d(TAG, "onAuthStateChanged: sign in"+user.getUid());
+                } else{
+                    Log.d(TAG, "onAuthStateChanged: sign out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+
+        mAuth.addAuthStateListener(mAuthStateListener);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if(mAuthStateListener!=null) mAuth.removeAuthStateListener(mAuthStateListener);
+
+    }
+
+
+    /**
+     * ----------------------firebase------------------------
+     */
+
 }
